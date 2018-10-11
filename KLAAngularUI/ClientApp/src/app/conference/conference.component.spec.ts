55aac11d87dfe6f6c25f99f67bc244dd8424e635
Bhavik Barot <bhavik.barot@nagarro.com>
2018-10-10 22:03:46 -0700

    Added conference component

---
+++ KLAAngularUI/ClientApp/src/app/conference/conference.component.spec.ts
@@ -0,0 +1,25 @@
+﻿/// <reference path="../../../../node_modules/@types/jasmine/index.d.ts" />
+import { TestBed, async, ComponentFixture, ComponentFixtureAutoDetect } from '@angular/core/testing';
+import { BrowserModule, By } from "@angular/platform-browser";
+import { ConferenceComponent } from './conference.component';
+
+let component: ConferenceComponent;
+let fixture: ComponentFixture<ConferenceComponent>;
+
+describe('Conference component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ConferenceComponent ],
+            imports: [ BrowserModule ],
+            providers: [
+                { provide: ComponentFixtureAutoDetect, useValue: true }
+            ]
+        });
+        fixture = TestBed.createComponent(ConferenceComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('should do something', async(() => {
+        expect(true).toEqual(true);
+    }));
+});
---
+++ KLAAngularUI/ClientApp/src/app/conference/conference.component.ts
@@ -0,0 +1,14 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-conference',
+    templateUrl: './conference.component.html',
+    styleUrls: ['./conference.component.scss']
+})
+/** Conference component*/
+export class ConferenceComponent {
+    /** Conference ctor */
+    constructor() {
+
+    }
+}
